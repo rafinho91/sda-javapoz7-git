fc3f472842f14a4adc9b7c81dcdfdd4fa9995cd7
RafalDrgas <rafinho91@gmail.com>
2018-03-15 18:32:47 +0100

    calculator new feature

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -1,7 +1,14 @@
 package sda;
 
+// tworzymy nowy branch na githubie
+// git pull
+// git checkout >nazwa nowego brancha<
+
+import sda.calculator.Calculator;
+
 public class Application {
     public static void main(String[] args) {
-
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(5,10));
     }
 }
---
+++ src/sda/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package sda.calculator;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
