1df4aadbe2533d15f7f3348daae0e0a3b4cb9007
RafalDrgas <rafinho91@gmail.com>
2018-03-15 20:43:53 +0100

    Merge branch 'factorial'
    # Conflicts:
    #	src/sda/Application.java

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -4,25 +4,27 @@ package sda;
 // git pull
 // git checkout >nazwa nowego brancha<
 //git merge >nazwa galezi do domerdzowania, trzeba byc na tej ostatecznej
+//git stash  git stash pop
 
 import sda.calculator.Calculator;
 import sda.calculator.RandomGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        Calculator myCalculator = new Calculator();
 
-        System.out.println(calculator.sum(5,10));
-        System.out.println(calculator.divide(5,10));
-        System.out.println(calculator.subtract(5,10));
-        System.out.println(calculator.multiply(5,10));
-        System.out.println(calculator.divide(5,0));
+        System.out.println(myCalculator.sum(5,10));
+        System.out.println(myCalculator.divide(5,10));
+        System.out.println(myCalculator.subtract(5,10));
+        System.out.println(myCalculator.multiply(5,10));
+        System.out.println(myCalculator.divide(5,0));
 
         RandomGenerator randomGenerator = new RandomGenerator();
         System.out.println(randomGenerator.getBigNumber());
         System.out.println(randomGenerator.getSmallNumber());
         // git checkout -b factorial -> tworzy branch
-        System.out.println("Factorial : " + calculator.factorial(3));
+        System.out.println("Factorial : " + myCalculator.factorial(3));
+        System.out.println();
+        System.out.println("blablabla");
     }
-
 }
