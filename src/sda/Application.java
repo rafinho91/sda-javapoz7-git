ff76941cc56f33b8e6b489c88bbd84a5eb795ed8
RafalDrgas <rafinho91@gmail.com>
2018-03-15 20:17:41 +0100

    test

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -10,16 +10,17 @@ import sda.calculator.RandomGenerator;
 
 public class Application {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        Calculator myCalculator = new Calculator();
 
-        System.out.println(calculator.sum(5,10));
-        System.out.println(calculator.divide(5,10));
-        System.out.println(calculator.subtract(5,10));
-        System.out.println(calculator.multiply(5,10));
-        System.out.println(calculator.divide(5,0));
+        System.out.println(myCalculator.sum(5,10));
+        System.out.println(myCalculator.divide(5,10));
+        System.out.println(myCalculator.subtract(5,10));
+        System.out.println(myCalculator.multiply(5,10));
+        System.out.println(myCalculator.divide(5,0));
 
         RandomGenerator randomGenerator = new RandomGenerator();
         System.out.println(randomGenerator.getBigNumber());
         System.out.println(randomGenerator.getSmallNumber());
+        System.out.println();
     }
 }
