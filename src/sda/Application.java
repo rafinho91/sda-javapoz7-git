c5af402ac8b5b674a28d9e6a26bdfe245ca029d5
RafalDrgas <rafinho91@gmail.com>
2018-03-15 18:38:27 +0100

    calculator advanced features

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -10,5 +10,9 @@ public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println(calculator.sum(5,10));
+        System.out.println(calculator.divide(5,10));
+        System.out.println(calculator.subtract(5,10));
+        System.out.println(calculator.multiply(5,10));
+        System.out.println(calculator.divide(5,0));
     }
 }
--- src/sda/calculator/Calculator.java
+++ src/sda/calculator/Calculator.java
@@ -4,4 +4,20 @@ public class Calculator {
     public int sum(int a, int b) {
         return a + b;
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0){
+            System.out.println("Invalid operation");
+            return 0;
+        }
+        return a / b;
+    }
 }
